6eb1df8851595303eb804f50ffeeb0e2afe099f1
Imad <imadouairem96@gmail.com>
2023-09-05 19:03:37 +0100

    temporary solution to pass selected recipe data

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,9 +7,17 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  @Output() selectedRecipe = new EventEmitter<Recipe>();
+
+
   recipes: Recipe[] = [
-    new Recipe("A Test Reciope", "This is a test data", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwEYYBbD33ig9Pbcel7iOhKc_wUt6k2wiFrg&usqp=CAU"),
-    new Recipe("A Test Reciope", "This is a test data", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwEYYBbD33ig9Pbcel7iOhKc_wUt6k2wiFrg&usqp=CAU")
+    new Recipe("Pizza", "This is the best pizza", "https://www.allrecipes.com/thmb/fFW1o307WSqFFYQ3-QXYVpnFj6E=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/48727-Mikes-homemade-pizza-DDMFS-beauty-4x3-BG-2974-a7a9842c14e34ca699f3b7d7143256cf.jpg"),
+    new Recipe("lazania", "This is the best Lazania", "https://ocdn.eu/pulscms-transforms/1/53wk9kpTURBXy82YTJmNDkxZGY5N2RhYWFlNDIxOWY2MzE2MmI3ZjM0ZS5qcGeTlQMEMc0X6M0Nc5MJpjY2M2M1ZgaTBc0EsM0Cdt4AAaEwAQ/wloska-lazania-zdjecie-ilustracyjne.jpg")
   ];
+
+
+  onRecipeSelected(recipe: Recipe) {
+    this.selectedRecipe.emit(recipe);
+  }
 
 }
